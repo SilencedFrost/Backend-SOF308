2059ed2c6d03be3ea57488d55d20695b38459574
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-07-20 23:07:22 +0700

    basic vuejs lab 3 components

--- Result/SOF308-Lab3/src/main.js
+++ Result/SOF308-Lab3/src/main.js
@@ -1,6 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+//Router
+import Home from './components/Home.vue'
+import Post from './components/Post.vue'
+import PostDetail from './components/PostDetail.vue'
+
+const routes = [
+    { path: '/', component: Home},
+    { path: '/post', component: Post},
+    { path: '/post/detail', component: PostDetail}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
