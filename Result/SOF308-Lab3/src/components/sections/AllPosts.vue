673feaddf198e30ac2242207fe3a7b009bd1c234
SilencedFrost <thnrgbefv0987@gmail.com>
2025-07-22 13:46:22 +0700

    finished Homepage

---
+++ Result/SOF308-Lab3/src/components/sections/AllPosts.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+    <div class="container-fluid p-0">
+        <h2 class="text-info">Tất cả bài viết</h2>
+        <div class="row g-2">
+            <div class="col-6">
+                <div class="card">e</div>
+            </div>
+            <div class="col-6">
+                <div class="card">e</div>
+            </div>
+        </div>
+        <div class="row g-2 mt-0">
+            <div class="col-6">
+                <div class="card">e</div>
+            </div>
+            <div class="col-6">
+                <div class="card">e</div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ Result/SOF308-Lab3/src/components/sections/TopicList.vue
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<template>
+    <h3 class="text-info">Chuyên mục</h3>
+    <ul class="list-group">
+        <li class="list-group-item active bg-info d-flex justify-content-between align-items-center">
+            <a class="nav-link" href="">Chế độ dinh dưỡng</a>
+            <span class="badge bg-primary rounded-pill">12</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a class="nav-link" href="">Chế độ tập</a>
+            <span class="badge bg-primary rounded-pill">15</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a class="nav-link" href="">Cẩm nang cuộc sống</a>
+            <span class="badge bg-primary rounded-pill">99</span>
+        </li>
+    </ul>
+</template>
