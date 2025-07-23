c353cee880507d8025caafa048e9f79702cf0e3d
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-07-23 20:21:10 +0700

    finished Lab3

--- Result/SOF308-Lab3/src/components/posts/PilatesPost.vue
+++ Result/SOF308-Lab3/src/components/posts/PilatesPost.vue
@@ -11,14 +11,12 @@ export default {
 </script>
 
 <template>
-    <h6 class="bg-light m-0 p-2">
+    <h5 class="bg-light m-0 p-2">
         Phương pháp tập Pilates giúp phục hồi chấn thương cột sống
         <img src="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/icons/chat-left.svg" alt="comments" width="20" height="20" class="me-1">
         16
-    </h6>
+    </h5>
     <hr class="m-0">
     <img class="p-2" :src="pilates" alt="">
-    <p class="p-2">
-        Ngoài việc giúp duy trì vóc dáng, cải thiện sự linh hoạt, dẻo dai cho người tập, Pilates giúp cơ bắp và các khớp mọc dài hơn, kéo giãn hơn...
-    </p>
+    <p class="p-2">Ngoài việc duy trì vóc dáng, cải thiện sự linh hoạt, dẻo dai, các bài tập Pilates còn có khả năng trị đau lưng, là giải pháp hữu hiệu để phục hồi chấn thương cột sống.</p>
 </template>
