6ce50abc39c3e40d28319243fbbad7790275e3eb
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-08-12 21:43:12 +0700

    finished lab8

---
+++ Result/SOF308-Lab8/src/pages/BlogPost.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const postId = computed(() => route.params.id)
+</script>
+
+<template>
+  Blogpost
+  <h5>Post ID: {{ postId }}</h5>
+</template>
